8fb45a50f6e06a6f288c285a71076b2618caccda
Matthew <turbodieseldodge@gmail.com>
2022-12-19 08:09:15 -0600

    commit tree 3a23f280b64100772e4f118cd40acd969a85ab9d

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const guests = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(guests) {
+    const thankYous = []
+    for (let person = 0; person < guests.length; person++){
+        thankYous.push(`Thank you, ${guests[person]}, for the wonderful surprise gift!`)
+    }
+    return thankYous;
+}
+writeCards(guests)
+
+
+function countDown(factors) {
+    while (factors >= 0) {
+        console.log(factors);
+        factors--;
+    }
+
+}
+countDown(factors)
